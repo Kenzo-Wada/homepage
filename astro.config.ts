be92950b064aae58f43e6ff375dc52fc9eb51200
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-02-25 01:43:34 +0900

    add site maps

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()],
+  integrations: [compress(), sitemap()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     imageService: true,
     isr: true,
-    functionPerRoute: true,
+    functionPerRoute: true
   })
 });
