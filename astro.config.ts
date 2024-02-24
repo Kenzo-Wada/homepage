0327fd9ce0bf8b3b26531cfa32dde838461c4db0
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-02-25 01:30:00 +0900

    add astro vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import compress from "astro-compress";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
-})
+  integrations: [compress()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    isr: true,
+    functionPerRoute: true,
+  })
+});
